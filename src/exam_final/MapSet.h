b3998404ea4b18952ccc9322749a4852251e756b
ivanasen <ivanasenchakarov@gmail.com>
2019-06-19 10:42:36 +0300

    Remove maps and sets fron final exam

--- src/exam_final/MapSet.h
+++ src/exam_final/MapSet.h
@@ -1,22 +1,57 @@
 #pragma once
 
-#include <unordered_map>
-#include <unordered_set>
+#include <vector>
 
 template<typename U, typename V>
 class MapSet {
 public:
     void add(const U &key, const V &value) {
-        if (_function.count(key) == 0) {
-            _function.emplace(key, std::unordered_set<V>());
+        int keyIndex = getKeyIndex(key);
+
+        if (keyIndex < _keys.size()) {
+            for (int i = 0; i < _values[keyIndex].size(); i++) {
+                if (_values[keyIndex][i] == value) {
+                    return;
+                }
+            }
+            _values[keyIndex].push_back(value);
+        } else {
+            _keys.push_back(key);
+            _values.push_back(std::vector<V>());
+            _values[_values.size() - 1].push_back(value);
         }
-        _function[key].insert(value);
+
     }
 
     bool hasValue(U key, const V &value) {
-        return _function.count(key) && _function[key].count(value);
+        for (int i = 0; i < _keys.size(); i++) {
+            if (_keys[i] == key) {
+
+                if (i >= _values.size()) {
+                    return false;
+                }
+
+                for (int j = 0; j < _values[i].size(); j++) {
+                    if (_values[i][j] == value) {
+                        return true;
+                    }
+                }
+                break;
+            }
+        }
+        return false;
     }
 
 private:
-    std::unordered_map<U, std::unordered_set<V>> _function;
+    std::vector<U> _keys;
+    std::vector<std::vector<V>> _values;
+
+    int getKeyIndex(const U &key) {
+        for (int i = 0; i < _keys.size(); i++) {
+            if (_keys[i] == key) {
+                return i;
+            }
+        }
+        return _keys.size();
+    }
 };
